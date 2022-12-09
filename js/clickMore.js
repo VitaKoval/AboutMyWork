31841e69f653d4504b1bf80e0812cf2a90547ce7
Vita Koval <mayevit@hotmail.com>
2022-12-09 17:13:30 +0200

    fix reting

--- js/clickMore.js
+++ js/clickMore.js
@@ -1,6 +1,6 @@
 const moreInfoProject = document.querySelector('.portfolio__button');
 const maxInfo = document.querySelector('.portfolio__max-info');
-const arrow = document.querySelector('.arrow');
+const arrow = document.querySelector('.arrow-group');
 
 moreInfoProject.addEventListener('click', openInfoMore);
 
@@ -8,10 +8,11 @@ function openInfoMore(evt) {
     maxInfo.classList.toggle('active');
 
     if (maxInfo.classList.contains('active')) {
-         moreInfoProject.textContent = 'Less';
+        moreInfoProject.textContent = 'Less';
+        arrow.style.display = "none";
     } else {
         moreInfoProject.textContent = 'More';
-      
+        arrow.style.display = 'block';
     }
 
    
