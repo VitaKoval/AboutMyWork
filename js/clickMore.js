6ee587db2667eab2c86e04c7a10648a42372c457
Vita Koval <mayevit@hotmail.com>
2022-12-09 13:37:24 +0200

    fixed button more

--- js/clickMore.js
+++ js/clickMore.js
@@ -1,8 +1,19 @@
 const moreInfoProject = document.querySelector('.portfolio__button');
 const maxInfo = document.querySelector('.portfolio__max-info');
+const arrow = document.querySelector('.arrow');
 
 moreInfoProject.addEventListener('click', openInfoMore);
 
 function openInfoMore(evt) {
     maxInfo.classList.toggle('active');
+
+    if (maxInfo.classList.contains('active')) {
+         moreInfoProject.textContent = 'Less';
+    } else {
+        moreInfoProject.textContent = 'More';
+      
+    }
+
+   
+
 }
